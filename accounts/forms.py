88498ada539b70c88c28ce3573f8c225f8c941f3
Vladislav Sapronov <vladislav.sap.sap@gmail.com>
2022-12-01 01:47:00 +0300

    -Lesson 22

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserRegistrationForm(forms.ModelForm):
+    username = forms.CharField()
+    email = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    password = forms.CharField()
+    password2 = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+    def clean_password2(self):
+        data = self.cleaned_data
+        if data['password'] != data['password2']:
+            raise forms.ValidationError("Password didn't match!")
+        return data['password2']
